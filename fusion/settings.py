ffac1c19a94b6150136e5859f30e5a13cdf6412e
Leonardo Nunes <leonardo21.nunes@gmail.com>
2022-08-02 23:28:15 -0300

    Debug

--- fusion/settings.py
+++ fusion/settings.py
@@ -143,6 +143,7 @@ DEFALT_FROM_EMAIL = 'contato@fusion.com.br'
 
 LOGOUT_REDIRECT_URL = 'index'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
